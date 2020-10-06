361de0e7b407964e1650174fae440608e365d5ad
inquis1t0r <piotr.kazimierski92@wp.pl>
2020-10-06 19:20:11 +0200

    Account abstract class added

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YurOTSharp
+{
+    abstract class Account
+    {
+        UInt32 accnumber;
+        int accType;     // ?
+        int premDays;    // Premium days
+
+        string name; //accnumber? - Protocol dependent 
+        String password;
+
+        List<string> charList;
+    }
+}
